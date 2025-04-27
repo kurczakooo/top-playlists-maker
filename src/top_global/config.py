9854a828484ab39b4ccc5cd9c53ef52bb1a83018
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2025-04-27 17:48:00 +0200

    added logger and fixed workflow

--- src/top_global/config.py
+++ src/top_global/config.py
@@ -2,6 +2,26 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
+import logging
+
+def setup_logger(filename: str):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # delete handlers if there are any
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    # Format 
+    log_format = f"%(asctime)s | %(levelname)s | {filename} | %(message)s\n"
+    date_format = "%Y-%m-%d %H:%M:%S"
+
+    # set handler
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(fmt=log_format, datefmt=date_format))
+    logger.addHandler(handler)
+
+    return logger
 
 load_dotenv()
 id = os.getenv('ID')
