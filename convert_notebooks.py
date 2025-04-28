36330b074572f5c23427a804cf332ccc522e184b
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2025-04-28 19:26:08 +0200

    another fix

--- convert_notebooks.py
+++ convert_notebooks.py
@@ -1,18 +1,23 @@
 import os
 import subprocess
 
+target_path = 'src/target'
+
 def convert_notebooks_to_py(directory='.'):
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.ipynb') and not file.startswith('.'):
                 notebook_path = os.path.join(root, file)
                 print(f"Converting {notebook_path} to .py...")
+                
                 subprocess.run([
-                    'jupyter', 'nbconvert', '--to', 'script', notebook_path
+                    'jupyter', 'nbconvert', 
+                    '--to', 'script',
+                    '--output-dir', target_path, 
+                    notebook_path
                 ])
+                
                 print(f'Success in converting {notebook_path}.')
-                # os.remove(notebook_path)
-                # print(f"Deleted {notebook_path}")
 
 if __name__ == "__main__":
     convert_notebooks_to_py()
