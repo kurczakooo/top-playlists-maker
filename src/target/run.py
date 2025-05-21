94a6fdd473c0813aa6c9bf780e0ed6ac81f3ff0d
Kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2025-05-21 16:37:42 +0200

    Update run.py

--- src/target/run.py
+++ src/target/run.py
@@ -3,7 +3,8 @@ import subprocess
 workflow_scripts = [
     "src/target/top_200_global_daily.py",
     "src/target/top_100_global_daily.py",
-    "src/target/top_50_global_daily.py"
+    "src/target/top_50_global_daily.py",
+    "src/target/followers_reporting.py"
 ]
 
 for script in workflow_scripts:
